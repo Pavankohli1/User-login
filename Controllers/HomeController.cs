3cce162f2814b13ff3484ae4312dcf84a6bf6668
DESKTOP-K82A4TQ\DELL <github2024>
2024-01-26 19:55:40 +0530

    user-login

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Login_page.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_page.Controllers
+{
+    public class HomeController : Controller
+    {
+       login1Entities db = new login1Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user log)
+        {
+            var user= db.users.Where(x => x.Username == log.Username && x.password == log.password).Count();
+            if (user>0)
+            {
+                return RedirectToAction("Dashboard");
+
+            }
+            else
+            {
+                return View();
+
+            }
+
+            
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
